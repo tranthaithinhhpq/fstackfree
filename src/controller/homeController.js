edd057e441888366bcf7e8b7d057c58843087dcb
Tran Quang Thai <tranthaithinhhpq@gmail.com>
2022-07-11 17:06:57 +0700

    video 27 edit user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -26,9 +26,31 @@ let displayGetCRUD = async (req, res) => {
   console.log("----------------------");
   console.log(data);
   console.log("----------------------");
-  return res.render("displayCRUD.ejs",{
-    dataTable: data
+  return res.render("displayCRUD.ejs", {
+    dataTable: data,
   });
+};
+
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  if (userId) {
+    let userdata = await CRUDService.getUserInfoById(userId);
+    console.log("---------------------------------------------");
+    return res.render("editCRUD.ejs", {
+      userdata: userdata,
+    });
+  } else {
+    return res.send("user not found");
+  }
+};
+
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  let allUsers = await CRUDService.updateUserdata(data);
+  // return res.send("update done");
+   return res.render("displayCRUD.ejs", {
+     dataTable: allUsers,
+   });
 };
 
 module.exports = {
@@ -36,4 +58,6 @@ module.exports = {
   getCRUD: getCRUD,
   postCRUD: postCRUD,
   displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
 };
--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -47,7 +47,49 @@ let getAllUser = () => {
   });
 };
 
+let getUserInfoById = (userId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: userId },
+        raw: true,
+      });
+
+      if (user) {
+        resolve(user);
+      } else {
+        resolve([]);
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let updateUserdata = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: data.id },
+      });
+
+      if (user) {
+        user.firstName = data.firstName;
+        user.lastName = data.lastName;
+        user.address = data.address;
+        await user.save();
+        let allUsers = await db.User.findAll();
+        resolve(allUsers);
+      } else {
+        resolve();
+      }
+    } catch (e) {}
+  });
+};
+
 module.exports = {
   createNewuser: createNewuser,
   getAllUser: getAllUser,
+  getUserInfoById: getUserInfoById,
+  updateUserdata: updateUserdata,
 };
